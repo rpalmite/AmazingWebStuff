d603336e043bc49c288be1d706d6cf5a4dd568d5
Steven Eberling <seberlin@steven-eberlings-computer.local>
2009-10-09 16:59:58 -0700

    build utility for client, should do things like concatenating, lint for js, running headless rhino unit tests on js source, minification, etc

---
+++ clientBuild.py
@@ -0,0 +1,30 @@
+#! /usr/local/bin/python
+
+import os
+import sys
+import getopt
+
+args = sys.argv
+
+def usage():
+	print "that a'int right"
+
+try:
+	opts, args = getopt.getopt(sys.argv, "", [])
+except getopt.GetoptError:
+	usage()
+	sys.exit(2)
+
+#refresh local depot
+os.chdir('Users/seberlin/Dev/AmazingWebStuff')
+os.system('git pull')
+
+ticketDemoHTML = ['viewTicket.html', 'ticketHistory.html', 'newTicket.html']
+
+def putText(srcDir, destDir, htmlFiles):
+	wutURL = 'http://api.webutilitykit.com:8000'
+	for file in htmlFiles:
+		os.system('cat ' + srcDir +  file + '| curl -X PUT -H \'Content-type: text/xml\' --data-binary @- ' + wutURL + destDir + '?pagename=' + file)	
+	return
+
+putText('/Users/seberlin/Sites/wut/', '/resource/html/pages', ticketDemoHTML)
