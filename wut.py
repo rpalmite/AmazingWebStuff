df88a982d584986087c43e697e4ac9a15f79d4b8
Steven Eberling <seberlin@steven-eberlings-computer.local>
2009-10-16 15:31:10 -0700

    login and logout bugfixing

--- wut.py
+++ wut.py
@@ -1,12 +1,10 @@
 #! /usr/local/bin/python
 
 import os
+from shutil import copyfile
 import sys
 import getopt
 
-args = sys.argv
-
-#
 # Configuration
 #
 
@@ -14,12 +12,13 @@ localRepo = '/Users/seberlin/Dev/AmazingWebStuff'
 localServ = '/Users/seberlin/Sites/wut'
 remoteServ = 'http://api.webutilitykit.com:8000'
 remoteHTMLDir = '/resource/html/pages'
+ticketFiles = ['viewTicket.html', 'ticketHistory.html', 'newTicket.html', 'login.html', 'wut.js']
 
 def doInDir(dir, fn):
-	def function(x):
+	def function(*args):
 		cwd = os.getcwd()
 		os.chdir(dir)
-		fn(x);
+		fn(*args); # Call fn with unpacked tuple
 		os.chdir(cwd)
 	return function
 
@@ -27,40 +26,43 @@ def executeCommands(cmdList):
 	for cmd in cmdList:
 		os.system(cmd)
 
-#
 # Command Line Parameters
 #
 
+args = sys.argv#
 def usage():
 	print "ERROR: invalid parameters"
 
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "m:x", ["--message="])
+	opts, args = getopt.getopt(sys.argv[1:], "c:xl", ["--commit=,--export,--export-local"])
 except getopt.GetoptError:
 	usage()
 	sys.exit(2)
 
-#
 # Refresh local Git repository & commit any changes
 #
 
 def commitLocal(m):
-	executeCommands(['git pull', 'git commit -a -m ' + m])
+	executeCommands(['git pull', 'git commit --interactive -m ' + m])
 
 for o, a in opts:
 	if o == '-m' or o == '--message=':
 		(doInDir(localRepo, commitLocal))(a)
 
-#
-# Export Ticketing System to the Webserver
+# Copy Repo Files to Local Server
 #
 
-ticketDemoHTML = ['viewTicket.html', 'ticketHistory.html', 'newTicket.html']
+for o, a in opts:
+	if o == '-l' or o == '--export-local':
+		for file in ticketFiles:
+			(doInDir(localRepo, copyfile))(file, localServ + "/" + file)		
+# Export Repo files to Remote Server
+#
 
-def putText(srcDir, destDir, htmlFiles):
+def putText(htmlFiles, srcDir, destDir):
 	for file in htmlFiles:
 		os.system('cat ' + srcDir + "/" + file + '| curl -X PUT -H \'Content-type: text/xml\' --data-binary @- ' + remoteServ + destDir + '?pagename=' + file)	
 	return
 for o, a in opts:
 	if o == '-x' or o == '--export':
-		putText(localServ, remoteHTMLDir, ticketDemoHTML)
+		putText(ticketFiles, localRepo, remoteHTMLDir)
