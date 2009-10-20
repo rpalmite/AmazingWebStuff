d25b6d762e6097d80a32e4d9d353be4fa551942a
Steven Eberling <seberlin@steven-eberlings-computer.local>
2009-10-20 16:07:43 -0700

    updated to YUI3

--- wut.js
+++ wut.js
@@ -1,16 +1,13 @@
-var WUT = function(callback) {
-  YUI({
-    base:"../yui/build/",
-    combine: true,
-//    filter: "DEBUG",
-    timeout: 10000
-  }).use("io-queue", "io", "io-xdr", "json", "node", "event-custom", "datatype", "cookie", function(Y) {
-
-    var Utils = {
-      jsonStringify: function(obj) {
+var WUT = function(libs) {
+  var Utils = {};
+  if(libs) {
+    if(libs.YUI && libs.YUI.JSON && libs.YUI.JSON.stringify) {
+      Utils.jsonStringify = function(obj) {
         return Y.JSON.stringify(obj);
-      },
-      Cookie: {
+      };
+    }
+    if(libs.YUI && libs.YUI.Cookie) {
+      Utils.Cookie = {
         get: function(key, params) {
           return Y.Cookie.get(key, params);
         },
@@ -20,193 +17,195 @@ var WUT = function(callback) {
         remove: function(key) {
           return Y.Cookie.remove(key);
         }
-      },
-      io: function(endpoint, config) {
+      };
+    }
+    if(libs.YUI && libs.YUI.io && libs.YUI.io.queue) {
+      Utils.io = function(endpoint, config) {
         Y.io.queue(endpoint, config);
-      }
-    };
+      };
+    }
+  }
 
-    var request = function(options, params, callback, args) {
-      var endpoint = "http://api.webutilitykit.com:8000",
-        path,
-        amp = "",
-        data = "",
-        param,
-        token,
-        returnArgs;
+  var request = function(options, params, callback, args) {
+    var endpoint = "http://api.webutilitykit.com:8000",
+      path,
+      amp = "",
+      data = "",
+      param,
+      token,
+      returnArgs;
       
-      if(typeof options.resource !== 'undefined' && options.resource !== "") {
-        path = "/resource/json/" + options.resource;
+    if(typeof options.resource !== 'undefined' && options.resource !== "") {
+      path = "/resource/json/" + options.resource;
 
-        for(var key in params) { if(params.hasOwnProperty(key)) {
-          if(key && params[key]) {
-            value = Utils.jsonStringify(params[key]); 
-            data = data + amp + key + "=" + value;
-            amp = "&";
-          }
-        }}
-      }
+      for(var key in params) { if(params.hasOwnProperty(key)) {
+        if(key && params[key]) {
+          value = Utils.jsonStringify(params[key]); 
+          data = data + amp + key + "=" + value;
+          amp = "&";
+        }
+      }}
+    }
       
-      endpoint = endpoint + path + ((data)? "?" + data: "");
+    endpoint = endpoint + path + ((data)? "?" + data: "");
 
-      // Add authentication token to headers on every request (except login)
-      if(!options.headers) {
-        options.headers = {};
-      }
-      token = Utils.Cookie.get("token", {raw: true});
-      if(token) {
-        options.headers["AUTHENTICATION-TOKEN"] = token;
-      }
-      
-      returnArgs = {Y:Y, WUT:WUT, resource: options.resource, table: params.table, method: options.method};
-      returnArgs.args = args;
+    // Add authentication token to headers on every request (except login)
+    if(!options.headers) {
+      options.headers = {};
+    }
+    token = Utils.Cookie.get("token", {raw: true});
+    if(token) {
+      options.headers["AUTHENTICATION-TOKEN"] = token;
+    }
 
-      Utils.io(endpoint, {
-        method: options.method,
-        data: data,
-        headers: options.headers,
-        on: {
-          success: function (id, o, a) {
-            callback(id, 
-              eval("(" + o.responseText + ")"), 
-              returnArgs
-            ); 
-          }
-        },
-        context: this,
-        arguments: args
-      });
-    };
+    returnArgs = {Y:Y, WUT:WUT, resource: options.resource, table: params.table, method: options.method};
+    returnArgs.args = args;
+
+    Utils.io(endpoint, {
+      method: options.method,
+      data: data,
+      headers: options.headers,
+      on: {
+        success: function (id, o, a) {
+          callback(id, 
+            eval("(" + o.responseText + ")"), 
+            returnArgs
+          ); 
+        }
+      },
+      context: this,
+      arguments: args
+    });
+  };
     
-    // parseUri 1.2.2
-    // (c) Steven Levithan <http://blog.stevenlevithan.com/archives/parseuri>
-    // MIT License
-    // Breaks a URI into pieces
-    var parseUri = function(str, isStrict) {
-      var o = {
-        strictMode: ((isStrict === true)? true: false),
-        key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
-        q: {
-          name: "queryKey",
-          parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-        },
-        parser: {
-          strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-          loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
-        }
+  // parseUri 1.2.2
+  // (c) Steven Levithan <http://blog.stevenlevithan.com/archives/parseuri>
+  // MIT License
+  // Breaks a URI into pieces
+  var parseUri = function(str, isStrict) {
+    var o = {
+      strictMode: ((isStrict === true)? true: false),
+      key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+      q: {
+        name: "queryKey",
+        parser: /(?:^|&)([^&=]*)=?([^&]*)/g
       },
-      m = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
-      uri = {},
-      i = 14;
+      parser: {
+        strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+        loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+      }
+    },
+    m = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+    uri = {},
+    i = 14;
 
-      while (i--) uri[o.key[i]] = m[i] || "";
+    while (i--) uri[o.key[i]] = m[i] || "";
 
-      uri[o.q.name] = {}
-      uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
-        if ($1) uri[o.q.name][$1] = $2;
-      });
+    uri[o.q.name] = {}
+    uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+      if ($1) uri[o.q.name][$1] = $2;
+    });
 
-      return uri;
-    };
+    return uri;
+  };
   
-    var WUT = {
-      get: function(options, params, callback, args) {
-        options.method = "GET";
-        request(options, params, callback, args);
-        return this;
-      },
-      post: function(options, params, callback, args) {
-        options.method = "POST";
-        request(options, params, callback, args);
-        return this;
-      },
-      put: function(options, params, callback, args) {
-        options.method = "PUT";
-        request(options, params, callback, args);
-        return this;
-      },
-      del: function(options, params, callback, args) {
-        options.method = "DELETE";
-        request(options, params, callback, args);
-        return this;
-      },
-      currentUser: function() {
-        var username = Utils.Cookie.get("username", {raw: true});
-        var token = Utils.Cookie.get("token", {raw: true});
-        if(token) {
-          return {username: username, token: token};
+  var WUT = {
+    get: function(options, params, callback, args) {
+      options.method = "GET";
+      request(options, params, callback, args);
+      return this;
+    },
+    post: function(options, params, callback, args) {
+      options.method = "POST";
+      request(options, params, callback, args);
+      return this;
+    },
+    put: function(options, params, callback, args) {
+      options.method = "PUT";
+      request(options, params, callback, args);
+      return this;
+    },
+    del: function(options, params, callback, args) {
+      options.method = "DELETE";
+      request(options, params, callback, args);
+      return this;
+    },
+    currentUser: function() {
+      var username = Utils.Cookie.get("username", {raw: true});
+      var token = Utils.Cookie.get("token", {raw: true});
+      if(token) {
+        return {username: username, token: token};
+      } else {
+        return {};
+      }
+    },
+    login: function(username, password, success) {
+      var loginCallback = function(id, o, a) {
+        var today = new Date(),
+            tomorrow = new Date();
+        if(o.result.indexOf("message") !== 0) { // Response doesn't start with an error message
+          tomorrow.setDate(today.getDate() + 1);
+          Utils.Cookie.set("username", a.args.username, {expires: tomorrow, raw: true});
+          Utils.Cookie.set("token", o.result, {expires: tomorrow, raw: true});
+          success(id, o, a);
         } else {
-          return {};
+          success(id, o, a); // Todo: failure callback
         }
-      },
-      login: function(username, password, success) {
-        var loginCallback = function(id, o, a) {
-          var today = new Date(),
-              tomorrow = new Date();
-          if(o.result.indexOf("message") !== 0) { // Response doesn't start with an error message
-            tomorrow.setDate(today.getDate() + 1);
-            Utils.Cookie.set("username", a.args.username, {expires: tomorrow, raw: true});
-            Utils.Cookie.set("token", o.result, {expires: tomorrow, raw: true});
-            success(id, o, a);
+      };
+      this.post({resource: "authentication"}, {username: username, password: password}, loginCallback, {username:username});
+    },
+    logout: function(success) {
+      console.log("logout");
+      var token,
+          logoutCallback;
+      logoutCallback = function(id, o, a) {
+        console.log("logout callback");
+        if(o.result === "success") {
+          success(id, o, a);
+        } else {
+          success(id, o, a); // Todo: failure callback
+        }
+      };
+      token = Utils.Cookie.get("token", {raw: true});
+      Utils.Cookie.remove("username");
+      Utils.Cookie.remove("token");
+      this.del({resource: "authentication"}, {token: token}, logoutCallback);
+    },
+    URL: function(parts) {
+      return {
+        // todo: http://benalman.com/projects/jquery-url-utils-plugin/
+        isSameHost : function(url) {
+          return this.parts().host === url.parts().host;
+        },
+        parts : function() { 
+          if(parts.source) {
+            return parseUri(parts.source);
           } else {
-            success(id, o, a); // Todo: failure callback
+            return null;
           }
-        };
-        this.post({resource: "authentication"}, {username: username, password: password}, loginCallback, {username:username});
-      },
-      logout: function(success) {
-        console.log("logout");
-        var token,
-            logoutCallback;
-        logoutCallback = function(id, o, a) {
-            console.log("logout callback");
-          if(o.result === "success") {
-            success(id, o, a);
-          } else {
-            success(id, o, a); // Todo: failure callback
+        },
+        getQueryParams: function() {
+          var keyValuePairs = this.parts()["query"].split(/[&?]/g),
+              params = {},
+              m,
+              key,
+              i
+          for (i = 0, n = keyValuePairs.length; i < n; ++i) {
+            m = keyValuePairs[i].match(/^([^=]+)(?:=([\s\S]*))?/);
+            if (m) {
+              key = decodeURIComponent(m[1]);
+              params[key] = decodeURIComponent(m[2]);
+            }
           }
-        };
-        token = Utils.Cookie.get("token", {raw: true});
-        Utils.Cookie.remove("username");
-        Utils.Cookie.remove("token");
-        this.del({resource: "authentication"}, {token: token}, logoutCallback);
-      },
-      URL: function(parts) {
-        return {
-          // todo: http://benalman.com/projects/jquery-url-utils-plugin/
-          isSameHost : function(url) {
-            return this.parts().host === url.parts().host;
-          },
-          parts : function() { 
-            if(parts.source) {
-              return parseUri(parts.source);
-            } else {
-              return null;
-            }
-          },
-          getQueryParams: function() {
-            var keyValuePairs = this.parts()["query"].split(/[&?]/g),
-                params = {},
-                m,
-                key,
-                i
-            for (i = 0, n = keyValuePairs.length; i < n; ++i) {
-              m = keyValuePairs[i].match(/^([^=]+)(?:=([\s\S]*))?/);
-              if (m) {
-                key = decodeURIComponent(m[1]);
-                params[key] = decodeURIComponent(m[2]);
-              }
-            }
-            return params;
-          },
-          toString: function() {
-            if(parts.source) {
-              return parts.source + "";
-            }
+          return params;
+        },
+        toString: function() {
+          if(parts.source) {
+            return parts.source + "";
           }
         }
       }
-    };
-    return callback(Y, WUT);
-  });
+    }
+  };
+  return WUT;
 };
